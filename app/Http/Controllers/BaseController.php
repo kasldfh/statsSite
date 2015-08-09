0cc22f97d12b77565bc756b1830bda3359cfbd02
f41lurizer <forumf41l@gmail.com>
2015-08-08 23:56:51 -0500

    added wildcard cache delete method

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -21,4 +21,35 @@ class BaseController extends Controller {
         if(Auth::guest())
             Redis::set($key, $value);
     }
+
+    public function getCached($key)
+    {
+        if(Auth::guest())
+            return Redis::get($key);
+    }
+
+    public function cache($key, $value)
+    {
+        if(Auth::guest())
+            Redis::set($key, $value);
+    }
+
+    //remove keys by prefix, can be array or str
+    //make sure to use double quotes for inputs
+    public function cacheRemove($input)
+    {
+        //if there's just one thing
+        if(!is_array($input))
+            $input = [$input];
+
+        foreach($input as $str)
+        {
+            $keys = Redis::keys($str);
+            foreach($keys as $key)
+            { 
+                Redis::del($key);
+            }
+        }
+        dd($input);
+    }
 }
